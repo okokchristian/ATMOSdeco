a76ae2c6cf8510ac9f4bec1cc31cdce0e9cfaa3b
Christian <itecs@MacBook-Pro-de-Christian.local>
2026-09-01 17:29:45 -0300

    comoresion de fotos de productos con squoosh

--- js/productos.js
+++ js/productos.js
@@ -7,7 +7,7 @@ const PRODUCTOS = [
         costoEnvio: 150,
         descripcion: "Fabricada a pedido con impresión 3D en PLA sustentable, la Lámpara Ondas combina diseño contemporáneo con una luz cálida y difusa. Cada pieza conserva las texturas naturales del proceso, haciendo que ninguna sea exactamente igual a otra.",
         imagenes: [
-            "../img/IMG_1808.JPG",
+            "../img/IMG_1808.webp",
             "../img/IMG_1801.JPG"
         ],
         linkMercadoPago: {
