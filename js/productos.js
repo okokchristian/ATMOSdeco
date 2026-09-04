982b8686775c436f27d94ce1866a716eb46125f8
Christian <itecs@MacBook-Pro-de-Christian.local>
2026-09-04 17:44:51 -0300

    nuevos productos

--- js/productos.js
+++ js/productos.js
@@ -14,5 +14,69 @@ const PRODUCTOS = [
             retiro: "https://mpago.la/xxxxx",
             envio: "https://mpago.la/yyyyy"
         }
+    },
+    
+     {
+        id: "lampara-nami",
+        nombre: "Lámpara Nami",
+        precio: "$1.000 UYU",
+        precioNumero: 1000,
+        costoEnvio: 150,
+        descripcion: "La lámpara Nami ilumina tus espacios con una calidez envolvente. Su diseño contemporáneo de columna plisada está fabricado de manera sustentable utilizando un bioplástico derivado de fuentes vegetales. Su luz cálida crea un ambiente acogedor y responsable.",
+        imagenes: [
+            "../img/nami.jpeg",
+        ],
+        linkMercadoPago: {
+            retiro: "https://mpago.la/xxxxx",
+            envio: "https://mpago.la/yyyyy"
+        }
+    }, 
+
+    {
+        id: "lampara-brote",
+        nombre: "Lámpara Brote",
+        precio: "$1.000 UYU",
+        precioNumero: 1000,
+        costoEnvio: 150,
+        descripcion: "Introduce sofisticación en tu hogar con la lámpara Brote. Su icónica forma de hongo, impresa en bioplástico sustentable, transforma la luz en textura viva, creando una atmósfera cálida e íntima. Una pieza de diseño responsable y contemporáneo que une arte y consciencia en perfecta armonía.",
+        imagenes: [
+            "../img/brote.jpeg",
+        ],
+        linkMercadoPago: {
+            retiro: "https://mpago.la/xxxxx",
+            envio: "https://mpago.la/yyyyy"
+        }
+    },
+
+     {
+        id: "lampara-fluid",
+        nombre: "Lámpara Fluid",
+        precio: "$1.000 UYU",
+        precioNumero: 1000,
+        costoEnvio: 150,
+        descripcion: "Introduce movimiento en tu hogar con la lámpara Fluid. Su característica forma con ondas se crea utilizando exclusivamente un material amigable con el medio ambiente, derivado de fuentes vegetales. Una pieza de diseño contemporáneo que transforma la luz cálida en una atmósfera dinámica, combinando alto valor estético y responsabilidad ecológica.",
+        imagenes: [
+            "../img/fluid.jpeg",
+        ],
+        linkMercadoPago: {
+            retiro: "https://mpago.la/xxxxx",
+            envio: "https://mpago.la/yyyyy"
+        }
+    },
+
+         {
+        id: "lampara-prisma",
+        nombre: "Lámpara Prisma",
+        precio: "$1.000 UYU",
+        precioNumero: 1000,
+        costoEnvio: 150,
+        descripcion: "La lámpara Prisma transforma tus espacios con su luz cálida y envolvente. Fabricada exclusivamente con PLA sustentable (material amigable con el medio ambiente derivado de fuentes vegetales), es una pieza de diseño contemporáneo y responsabilidad ecológica, ideal para momentos de relajación.",
+        imagenes: [
+            "../img/prisma.jpeg",
+        ],
+        linkMercadoPago: {
+            retiro: "https://mpago.la/xxxxx",
+            envio: "https://mpago.la/yyyyy"
+        }
     }
 ];
